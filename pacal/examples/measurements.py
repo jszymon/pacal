28f180bc51584b40b75b0bedd97f75c634247f24
jszymon <jszymon@09d9ae67-c4dd-48c1-9f8a-f29d756ae481>
2011-02-11 12:01:43 +0000

    small fixes to the measurements example

--- pacal/examples/measurements.py
+++ pacal/examples/measurements.py
@@ -13,26 +13,38 @@ from pacal.distr import demo_distr
 X = UniformDistr(0,2)
 #Y = BetaDistr(2,2) 
 Y = BetaDistr(0.5,0.5) 
+X = UniformDistr(-1,1)
+Y = NormalDistr()
 X.summary()
 Y.summary()
 
-fun = lambda p: p*X + (1-p)*Y
+def fun(p):
+    p = numpy.squeeze(p)
+    print p
+    return p*X + (1-p)*Y
 
 pOptVar = fminbound(lambda p: fun(p).get_piecewise_pdf().var(), 0, 1, xtol = 1e-16)
 print "pOptVar = ", pOptVar 
-fun(pOptVar).summary()
+dopt = fun(pOptVar)
+dopt.summary()
+print "iqrange(0.025)=", dopt.get_piecewise_pdf().iqrange(0.05)
 
-pOptMad= fminbound(lambda p: fun(p).get_piecewise_pdf().medianad(), 0, 1, xtol = 1e-16)
+pOptMad = fminbound(lambda p: fun(p).get_piecewise_pdf().medianad(), 0, 1, xtol = 1e-16)
 print "pOptVar = ", pOptMad 
-fun(pOptMad).summary()
+dopt = fun(pOptMad)
+dopt.summary()
+print "iqrange(0.025)=", dopt.get_piecewise_pdf().iqrange(0.05)
 
-pOptIQrange = fminbound(lambda p: fun(p).get_piecewise_pdf().iqrange(0.05), 0, 1, xtol = 1e-16)
+pOptIQrange = fminbound(lambda p: fun(p).get_piecewise_pdf().iqrange(0.025), 0, 1, xtol = 1e-16)
 print "pOptIQrange = ", pOptIQrange 
-fun(pOptIQrange).summary()
+dopt = fun(pOptIQrange)
+dopt.summary()
+print "iqrange(0.025)=", dopt.get_piecewise_pdf().iqrange(0.05)
 
+print "-----------------------"
 figure()
-X.plot(color='k')
-Y.plot(color='k')
+#X.plot(color='k')
+#Y.plot(color='k')
 fun(pOptVar).plot(color='r')
 fun(pOptMad).plot(color='g')
 fun(pOptIQrange).plot(color='b')
