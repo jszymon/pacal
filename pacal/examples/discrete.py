4b82964c63ad9a310bb9e3d1ef2d39ae8a909109
mkorzen <mkorzen@09d9ae67-c4dd-48c1-9f8a-f29d756ae481>
2011-08-23 06:52:18 +0000

    discrete demo some examples

--- pacal/examples/discrete.py
+++ pacal/examples/discrete.py
@@ -6,14 +6,14 @@
 from pylab import figure, show, subplot
 
 from pacal import *
-from pacal.distr import demo_distr
-
+from pacal.distr import demo_distr, sign
+from matplotlib.pyplot import legend
 
 #
 #! Constuctor
 #!------------
 #!
-U = UniformDistr(0,1)
+U = UniformDistr(0, 1)
 M = DiscreteDistr(xi=[0.0, 1.0, 2.0, 3.0], pi=[0.3, 0.4, 0.2, 0.1])
 S = M + U
 SM = min(S, S)
@@ -41,7 +41,7 @@ SM.get_piecewise_cdf().plot()
 #! -------------------------------------
 b5 = ZeroDistr()
 for i in range(5):
-    b5 += DiscreteDistr(xi = [0, 1], pi = [0.2, 0.8])
+    b5 += DiscreteDistr(xi=[0, 1], pi=[0.2, 0.8])
 figure()
 subplot(131)
 b5.plot()
@@ -68,8 +68,8 @@ bd.get_piecewise_cdf().plot()
 
 #! Mixing continuous and discrete distributions
 #!----------------------------------------------
-d = DiscreteDistr(xi = [1, 2], pi = [0.2, 0.8])
-U = UniformDistr(0,2)
+d = DiscreteDistr(xi=[1, 2], pi=[0.2, 0.8])
+U = UniformDistr(0, 2)
 A1 = d + U
 A2 = d * U
 A3 = d / U
@@ -103,5 +103,65 @@ A1.get_piecewise_cdf().plot()
 A2.get_piecewise_cdf().plot()
 A3.get_piecewise_cdf().plot(xmax=6.0)
 A4.get_piecewise_cdf().plot()
+
+
+#! Mixture distributions
+#!----------------------
+
+d = DiscreteDistr(xi=[-1, 2, 6], pi=[0.2, 0.4, 0.4])
+N = NormalDistr()
+GM = d + N
+
+GM.summary()
+GM.plot()
+
+
+
+#! Sign, Abs 
+#! ----------
+figure()
+N = NormalDistr(0.1,1)
+S = sign(N)
+A = abs(N)
+S.plot(color='r', linewidth=2.0, label="sign(N)")
+A.plot(color='g', linewidth=2.0, label="abs(N)")
+N.plot(color='b', linewidth=2.0, label="N")
+legend()
+figure()
+demo_distr(S * A, theoretical=N)
+
+#! min, max, Conditional distributions
+#! ------------------------------------
+#!
+#! 
+
+figure()
+E = max(ExponentialDistr() - 1, ZeroDistr())
+subplot(211)
+E.plot(linewidth=2.0)
+E.summary()
+subplot(212)
+S = E + E + E + E + E + E
+S.plot(linewidth=2.0)
+S.summary()
+
+
+figure()
+
+E = ExponentialDistr()
+E1 = CondGtDistr(E, 1)
+E2 = CondLtDistr(E, 1)
+subplot(311)
+E.plot(linewidth=2.0)
+subplot(312)
+E1.plot(linewidth=2.0)
+E1.summary()
+subplot(313)
+E2.plot(linewidth=2.0)
+E2.summary()
+
+#! Memorylessness of exponential distribution
+figure()
+demo_distr(E1 - 1, theoretical=E)
 
 show()
