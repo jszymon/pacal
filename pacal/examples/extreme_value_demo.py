78c366b49a9be443d8136081c5d0967bcd67ac0c
mkorzen <mkorzen@09d9ae67-c4dd-48c1-9f8a-f29d756ae481>
2011-09-12 18:24:44 +0000

    plot limits added

--- pacal/examples/extreme_value_demo.py
+++ pacal/examples/extreme_value_demo.py
@@ -18,10 +18,10 @@ def extreme_limit_demo(X, ns, an, bn, limit_distr, xmin = None, xmax = None, yma
         Y = iid_max(X, n)
         Y = an(n) * (Y - bn(n))
         #demo_distr(Y, g)
-        Y.plot(color=colors[i], label = "n=" + str(n))
+        Y.plot(xmin=xmin, xmax=xmax, color=colors[i], label = "n=" + str(n))
         Y.summary()
 
-    g.plot(linewidth=4, color = "m", alpha = 0.4, label = "limit")
+    g.plot(xmin=xmin, xmax=xmax, linewidth=4, color = "m", alpha = 0.4, label = "limit")
     g.summary()
 
     if xmin is not None:
@@ -35,13 +35,14 @@ def extreme_limit_demo(X, ns, an, bn, limit_distr, xmin = None, xmax = None, yma
 
 
 ns = [8, 16, 32, 64, 128, 256, 1024, 2048]
+#ns = [8, 16, 32]
 
 
 X = UniformDistr(0,1)
 an = lambda n: -n
 bn = lambda n: 1
 g = ExponentialDistr() # = WeibullDistr(1)
-extreme_limit_demo(X, ns, an, bn, g, xmax = 10)
+extreme_limit_demo(X, ns, an, bn, g, xmax = 5)
 
 # TODO: find bn here
 # X = ExponentialDistr()
@@ -54,12 +55,12 @@ X = NormalDistr()
 an = lambda n: sqrt(2*log(n))
 bn = lambda n: sqrt(2*log(n)) - 0.5 * (log(log(n)) + log(4*pi)) / sqrt(2*log(n))
 g = GumbelDistr()
-extreme_limit_demo(X, ns, an, bn, g)
+extreme_limit_demo(X, ns, an, bn, g, xmin=-3, xmax=5)
  
 X = CauchyDistr()
 an = lambda n: 1.0 / tan(pi/2 - pi/n)
 bn = lambda n: 0
 g = FrechetDistr(1)
-extreme_limit_demo(X, ns, an, bn, g)
+extreme_limit_demo(X, ns, an, bn, g, xmin=-1, xmax=5)
 
 show()
