47186382ab34061ebb3bed5aafddea7b2a8b4870
mkorzen <mkorzen@09d9ae67-c4dd-48c1-9f8a-f29d756ae481>
2011-12-22 15:57:25 +0000

    example: The Distribution of Linear Regression Coefficients

--- pacal/examples/dependent/linreg.py
+++ pacal/examples/dependent/linreg.py
@@ -1,106 +1,97 @@
+#!===================================================
+#! The Distribution of Linear Regression Coefficients
+#!===================================================
+
 from numpy import concatenate, polyfit
 
-from pylab import figure, show, plot, subplot
+from pylab import figure, show, plot, subplot, title
 
 
 from pacal import *
 from pacal.depvars.copulas import *
 from pacal.depvars.models import Model
 import time
+from pacal.utils import maxprob
 
 from scipy.optimize import fmin
 
 from numpy.random import seed
 seed(1)
 
-#params.interpolation_nd.maxn = 8
-
-n = 20
+params.interpolation_nd.maxn = 4
+params.interpolation.maxn = 20
+params.interpolation_pole.maxn = 20
+params.interpolation_nd.debug_info = False
+#$
+#$ $y_i=ax_i+e_i,\quad, i=0,\ldots,\n-1$
+#$
+n = 10
 X = []
 E = []
 Y = []
-#A = UniformDistr(0,1, sym = "A")
-#B = UniformDistr(0,1, sym = "B")
 t0 = time.time()
-
-Ei = MollifierDistr(0.4)
-Ei.summary()
-#Ei.plot()
-#show()
-
-#A = UniformDistr(0,1, sym = "A")
-#B = UniformDistr(0,1, sym = "B")
-A = BetaDistr(3,3, sym = "A")
-B = BetaDistr(3,3, sym = "B")
+A = UniformDistr(0,1, sym = "A")
+B = UniformDistr(0,1, sym = "B")
 for i in range(n):
-    #X.append(UniformDistr(0, 1, sym = "X{}".format(i)))
-    X.append(BetaDistr(3, 3, sym = "X{}".format(i)))
-    E.append(MollifierDistr(0.4, sym = "E{}".format(i)))    
+    X.append(UniformDistr(0, 1, sym = "X{0}".format(i)))
+    E.append(NormalDistr(0,0.2) | Between(-2,2))
+    E[i].setSym("E{0}".format(i)) 
     Y.append(A * X[i]  + B + E[i])
-    Y[i].setSym("Y{}".format(i))
+    Y[i].setSym("Y{0}".format(i))
 
 M = Model(X + E + [A, B], Y)
-#M.eliminate_other(X + E + [A, B] + Y)
-M.toGraphwiz()
-
-
-
+M.toGraphwiz(f=open('tmp.dot', mode="w+"))
+#!
+#! Sample
+#!
 Xobs = X[0].rand(n)
-a = 0.3 #A.rand(1)[0]
-Yobs = a*Xobs+0.7 + Ei.rand(n)
-
-b = 0.7
-Yobs = a*Xobs+0.7 + Ei.rand(n)
-print "a=", a
-(ar,br)=polyfit(Xobs,Yobs,1)
-#figure()
-#plot(Xobs, Yobs, 'o')
-
-
-# #M.eliminate_other(X + Y)
-# MXY = M.inference([X[0], Y[0]], [B], [0.7])
-# figure()
-# MXY.plot()
-# plot([0.0, 1.0], [0.7, 0.7+a], "k-", linewidth=2.0)
-# plot(Xobs, Yobs, "ko")
-# plot()
-# show()
-
-print ar, br
-print Xobs, Yobs
-print X + Y
-print concatenate((Xobs, Yobs))
-
-#print M
-#MAB = M.inference([A,B]+E, X + Y,  concatenate((Xobs, Yobs)))
-#print MAB
-#MAB = MAB.inference([A,B], X + Y,  concatenate((Xobs, Yobs)))
+a, b = 0.3, 0.7 
+Yobs = a * Xobs + b + E[0].rand(n)
+print '{0:{align}15}\t{0:{align}15}'.format("Xobs","Xobs", align = '>')
+for i in range(len(Xobs)):
+    print '{0:{align}20}\t{0:{align}14}'.format(Xobs[i], Yobs[i], align = '>')
+#!
+#! Model
+#!
+print M
+#!
+#! Inference
+#!
 MAB = M.inference([A,B], X + Y,  concatenate((Xobs, Yobs)))
 MA = MAB.inference([A],[],[])
 MB = MAB.inference([B],[],[])
-
-#M = M.inference([A,B], [X[0], Y[0]], [0.2, 0.4])
 print MAB
+print MA
+print MB
 figure()
 MAB.plot(have_3d=True)
+title("Joint distribution of A and B conditioned on sample")
+#show()
 figure()
 MAB.plot(have_3d=False, cont_levels=10)
-        
-print MA
+title("Joint distribution of A and B conditioned on sample")   
+#show()
 figure()
 subplot(211)
 MA.plot()
-print MB
+title("Marginalized distribution of A")
 subplot(212)
 MB.plot()
+title("Marginalized distribution of B")
+#show()
+#!
+#! Estimation coefficients
+#!
+print "original coefficients a=", a, "b=", b
 print "mean   est. A=", MA.as1DDistr().mean(),   "est. B=", MB.as1DDistr().mean()
 print "median est. A=", MA.as1DDistr().median(), "est. B=", MB.as1DDistr().median()
 print "mode   est. A=", MA.as1DDistr().mode(),   "est. B=", MB.as1DDistr().mode()
 MAB.nddistr(1, 3)
-
-#paropt=fmin(lambda x,y: -MAB.nddistr(x,y), [MA.as1DDistr().mode(), MB.as1DDistr().mode()])
-#print paropt
-#print MAB.nddistr(paropt)
-
-print time.time() - t0
+abopt = MAB.nddistr.mode()#maxprob(MAB.nddistr, array([0.5, 0.5]))
+(ar,br)=polyfit(Xobs,Yobs,1)
+print "              MAP =", abopt[0], abopt[1]
+print "polyfit (LSE) est =", ar, br
+print "time of doing=", time.time() - t0
 show()
+
+
