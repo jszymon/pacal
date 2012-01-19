d0b409104b525979015f7450f2ab980b31951a6f
mkorzen <mkorzen@09d9ae67-c4dd-48c1-9f8a-f29d756ae481>
2012-01-19 12:22:43 +0000

    more complicated example with control signal

--- pacal/examples/dependent/diffeq_noise.py
+++ pacal/examples/dependent/diffeq_noise.py
@@ -12,7 +12,8 @@ t0 = time.time()
 params.interpolation_nd.maxq = 7
 params.interpolation.maxn = 3
 params.interpolation_pole.maxn =3
-params.interpolation_nd.debug_info = False
+params.interpolation_nd.debug_info = True
+params.models.debug_info = True
 
 
 #!
@@ -23,17 +24,23 @@ params.interpolation_nd.debug_info = False
 #! O(i) = Y(i+1) + E(i), i=0,...,n-1
 #!
 
-A = BetaDistr(3, 3, sym="A")    # parameter of equation
+K = UniformDistr(0.5, 1.5, sym="A")    # parameter of equation
 Y0 = BetaDistr(2, 2, sym="Y0")  # initial value
+#Y0 = UniformDistr(0, 1, sym="Y0")  # initial value
 n = 5                           # number time points
 h = 1.0 / n
-K = (1 + h * A)
+#K = (1 - h * A)
 K.setSym("K") 
-Y = [Y0]                        # list of states
-O, E = [], []                   # lists of observations and errors
+Y, O, E, U  = [], [], [], []           # lists of states, observations and errors
 for i in xrange(n):
-    Y.append(Y[i] * K)
-    Y[i + 1].setSym("Y" + str(i + 1))  
+    #Y.append(Y[i] * K)
+    #Y[i + 1].setSym("Y" + str(i + 1))  
+    U.append(UniformDistr(-0.2,0.2, sym="U{0}".format(i)))
+    if i==0:
+        Y.append(Y0 * K + U[i])
+    else:
+        Y.append(Y[i-1] * K + U[i])
+    Y[-1].setSym("Y" + str(i+1))  
     ei = NormalDistr(0.0, 0.2) | Between(-1, 1)
     ei.setSym("E{0}".format(i))
     E.append(ei)
@@ -42,11 +49,11 @@ for i in xrange(n):
 #! 
 #! Model
 #! -----
-P = NDProductDistr([A, Y[0]] + E)
+P = NDProductDistr([K, Y0] + E + U)
 M = Model(P, O)
 print M
+#M.eliminate_other(E + Y + O + [A, Y0] + U)
 M.toGraphwiz(f=open('bn.dot', mode="w+"))
-M.eliminate_other(E + Y + O + [A])
 
 #!
 #! Joint distribution of initial condition and parameter of equation
@@ -54,8 +61,9 @@ M.eliminate_other(E + Y + O + [A])
 figure()
 i = 0
 ay0 = []
-for yend in [0.5, 1.5]:
-    M2 = M.inference(wanted_rvs=[A, Y[0]], cond_rvs=[O[-1]], cond_X=[yend])
+ui = [0.1]*n
+for yend in [0.3, 1.2]:
+    M2 = M.inference(wanted_rvs=[K, Y0], cond_rvs=[O[-1]] + U, cond_X=[yend] + ui)
     subplot(1, 2, i + 1)
     title("given that observation O[{0}]={1}".format(n-1, yend))
     M2.plot()
@@ -70,7 +78,7 @@ figure()
 for j in range(len(ay0)):
     ymean, ystd = [], []
     for i in range(n):
-        Myi = M.inference([O[i]], [A, Y[0]], ay0[j])
+        Myi = M.inference([O[i]], [K, Y[0]] + U, list(ay0[j]) + ui)
         ymean.append(Myi.as1DDistr().mean())
         ystd.append(Myi.as1DDistr().std())
     subplot(1, 2, j + 1)
