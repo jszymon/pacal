ce1a7ba44798e6501387a66ec2e2f4f0246ae7b4
mkorzen <mkorzen@09d9ae67-c4dd-48c1-9f8a-f29d756ae481>
2012-01-23 19:05:21 +0000

    one dimensional Kalman filter example

--- pacal/examples/dependent/kalman.py
+++ pacal/examples/dependent/kalman.py
@@ -1,85 +1,108 @@
 """Simple differential equation."""
 
 from pacal import *
-from pylab import figure, show, zeros, plot, legend
+from pylab import figure, show, zeros, plot, legend, subplot, rc
+
+from matplotlib.lines import Line2D
+
+rc('axes', labelsize=18)
+rc('xtick', labelsize=15.0)
+rc('ytick', labelsize=15.0)
+rc('legend', fontsize=17.0)
+
+
+linestyles = ["-", "--", "-.", ":"]
+pfargs = {"linewidth":3, "color":"k", "dash_joinstyle":"round"}
+
 
 from pacal.depvars.models import Model
 from pacal.depvars.nddistr import NDProductDistr, Factor1DDistr
-
-from numpy import pi, std, array
-params.interpolation_nd.maxn = 3
-params.interpolation.maxn = 20
-params.interpolation_pole.maxn = 20
-params.interpolation_nd.debug_info = True
-
-# y' = ay  Euler's method
-
-
-#A = BetaDistr(1, 1, sym="A")
-#A = UniformDistr(0.5, 1.75, sym="A")
-n = 1
-A = NormalDistr(0.5, 0.2) | Between(0.1, 0.9)
-A.setSym("A")
-A.parents = []
-#Y0 = BetaDistr(2, 2, sym="Y0")
-
-Y = [UniformDistr(-10, 10, sym="Y0")]
-U, E, O = [], [], []
+from numpy.random import seed
+seed(1)
+from numpy import pi, std, array, concatenate, mean, abs
+params.interpolation_nd.maxq = 2
+params.interpolation.maxn = 100
+params.interpolation_pole.maxn = 100
+params.interpolation_nd.debug_info = False
+params.interpolation.debug_info = False
+params.models.debug_info = False
+A = BetaDistr(3, 3, sym="A")    # parameter of equation
+#Y0 = BetaDistr(3, 3, sym="Y0")  # initial value
+Y0 = UniformDistr(-0.5, 0.5, sym="Y0")  # initial value
+n = 3      # number time points
+h = 1.0 / n
+#A =-0.3
+K = (1 - h * A)
+#K = BetaDistr(5, 3, sym="A") #0.8
+K0=0.7
+K= K0
+#K.setSym("K") 
+Y = []                        # list of states
+O, E, U = [], [], []            # lists of observations and errors
 for i in xrange(n):
-    U.append(UniformDistr(-2, 2, sym="U" + str(i)))
-    Y.append(Y[i])
-    Y[i + 1].setSym("Y" + str(i + 1))
-    #ei= BetaDistr(3,3, sym="E{0}".format(i))
-    ei = NormalDistr(0.0, 2.2) | Between(-1, 1)
-    m = ei.mean()
-    ei = NormalDistr(0.0, 2.2) - m | Between(-1 - m, 1 - m)
+    print i
+    U.append(UniformDistr(-0.2,0.2, sym="U{0}".format(i)))
+    if i==0:
+        Y.append(Y0 * K + U[i])
+    else:
+        Y.append(Y[i-1] * K + U[i])
+    Y[i].setSym("Y" + str(i+1))  
+    ei = NormalDistr(0.05, 0.1) | Between(-0.4, 0.4)
     ei.setSym("E{0}".format(i))
+    #ei = MollifierDistr(0.5, sym="E{0}".format(i))
     E.append(ei)
-    O.append(Y[i + 1] * A - E[i] + U[i])
-    O[i].setSym("O{0}".format(i)) 
-    #E = [BetaDistr(3,3,sym="E"+str(i)) for i in xrange(n+1)]
-     
-P = NDProductDistr([Y[0],A] + E + U)
-M = Model(P, O)
-M.eliminate_other(E + Y + O + U + [A])
-M.toGraphwiz()#f=open('bn.dot', mode="w+"))
+    O.append(Y[-1] + E[-1])
+    O[-1].setSym("O{0}".format(i))
+    #print O[-1].range(), O[-1].range_()
+M = Model(U+[Y0]+E, Y+O)
 print M
+M.toGraphwiz(f=open('bn.dot', mode="w+"))
 nt = 100
 u = zeros(nt)
 t = zeros(nt)
-Ymean, Ymode = [], []
-Ymedian = []
-Ynoise, Yorg = [], []
-yi = 0.3
+Yorg = zeros(nt)
+Ynoised = zeros(nt)
+Ydenoised = zeros(nt)
+Ydenoised1 = zeros(nt)
+Ydenoised2 = zeros(nt)
+Udenoised = zeros(nt)
+yi = 0.0
+ydenoise =  0.0
+ynoise  = 0.0
+y = 0.0
+figure()
 for i in range(nt):
     t[i] = i
-    u[i] = sign(sin(2 * pi * i / nt))
-    print [0.3, 0.3, u[i]]
-    #MYi = M.inference([O[0]], [Y[0], A, U[0]], [0.3, 0.3, u[i]]).as1ddistr()
-    MYi = M.inference([O[0]], [Y[0], U[0],A], [yi, u[i],0.5]).as1DDistr()
-    Ymean.append(MYi.mean()) 
-    Ymode.append(MYi.mode()) 
-    Ymedian.append(MYi.median())
-    if len(Ynoise) == 0:
-        Yorg.append(yi + u[i])
-        Ynoise.append(yi + u[i] + float(E[0].rand()))
-    else:
-        Yorg.append(Yorg[-1] * 0.6 + u[i])
-        Ynoise.append(Ynoise[-1] * 0.6 + u[i] + float(E[0].rand()))
-    yi = Ymean[-1]
-    print i, yi, yi, u[i], float(E[0].rand())
+    print "===========================================", i
+    u[i] = 0.1*sign(sin(4 * pi * i / nt))
+    #u[i] = 0.1*(sin(4 * pi * i / nt))
+    y = y*K0 + u[i]
+    Yorg[i] = y
+    Ynoised[i] = y + E[0].rand()  
     
-plot(t, u, 'k', label="U")
-plot(t, Ymean, 'r', label="Ymean")
-#plot(t, Ymode,'r')
-plot(t, Ymedian, 'g', label="Ymedian")
-plot(t, Ynoise, 'k', label="Ynoise")
-plot(t, Yorg, 'b', label="Yorg")
-legend()
-YnoiseK = E[0].rand(nt) + Ymean
-plot(t, YnoiseK, 'r--')
-print std(array(Yorg) - Ynoise)
-print std(YnoiseK - Yorg)
-print std(array(Ymean) - Yorg)
-print std(array(Ymedian) - Yorg)
+    if i>n-1:
+        MY = M.inference([Y[-1]], O + U , concatenate((Ynoised[i-n+1:i+1], u[i-n+1:i+1])))
+        ydenoised = MY.as1DDistr().mean()
+        ydenoised1 = MY.as1DDistr().median()
+        Ydenoised[i] = ydenoised
+        Ydenoised1[i] = ydenoised1
+        #MY.as1DDistr().boxplot(i, useci=0.05)
+        
+    print range(i-n+1,i+1,1)
+    print i, Yorg[i], Ynoised[i], Ydenoised[i], Ydenoised1[i] 
+#figure()
+plot(t, u, 'k-', label="U", linewidth = 1.0)
+plot(t, Ynoised, 'k.--', label="O", linewidth = 1.0)
+plot(t, Yorg, 'k:', label="Y original", linewidth = 3.0)
+#plot(t, Ydenoised, 'r-', label="X1 denoised")
+plot(t, Ydenoised1, 'k-', label="Y denoised", linewidth = 2.0)
+#plot(t, Ydenoised2, 'b-', label="X1 mode denoised")
+#plot(t, Udenoised, 'b--', label="U1 denoised")
+legend(loc='lower left')
+print sqrt(mean((Yorg-Ynoised)**2)), sqrt(mean((Yorg-Ydenoised)**2)) 
+print mean(abs(Yorg-Ynoised)), mean(abs(Yorg-Ydenoised)) 
+
+print sqrt(mean((Yorg-Ynoised)**2)), sqrt(mean((Yorg-Ydenoised1)**2)) 
+print mean(abs(Yorg-Ynoised)), mean(abs(Yorg-Ydenoised1)) 
+
 show()
