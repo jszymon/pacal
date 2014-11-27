4cfdc05574be84a73105b7b9f37ad6cadd96dfb3
Szymon Jaroszewicz <jszymon@gmail.com>
2014-11-27 22:51:45 +0100

    disable openblas threading in parallel mode

--- pacal/__init__.py
+++ pacal/__init__.py
@@ -101,4 +101,9 @@ if params.general.parallel:
     import copy_reg
     #copy_reg.pickle(_np.ufunc, _pickle_ufunc, _unpickle_ufunc)
     copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
-    
+    # disable threading in openblas
+    import os
+    try:
+        os.environ["OPENBLAS_NUM_THREADS"] = "1"
+    except:
+        print "WARNING: could not disable openblas threading"
